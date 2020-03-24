c83d1923551ebfda14c18a2f011cc6cd9ca3a055
nibogcorp <nibogcorp@gmail.com>
2020-03-24 05:07:22 +0300

    Coddons Added

--- coddon.js
+++ coddon.js
@@ -1,8 +1,103 @@
 class Coddon{
-	constructor(x, y, id, letters, size){
+	constructor(x, y, id, coddon, size){
 		this.position = createVector(x, y);
 		this.id = id;
-		this.letters = letters; 
+		this.coddon = coddon; 
 		this.size = size;
+	}
+	show(){
+		if(this.coddon === "A") {
+			fill(120, 0, 0);
+		}else if(this.coddon === "R") {
+			fill(255, 0, 0);
+		}else if(this.coddon === "N") {
+			fill(0, 120, 0);
+		}else if(this.coddon === "D") {
+			fill(0, 255, 0);
+		}else if(this.coddon === "C") {
+			fill(0, 0, 120);
+		}else if(this.coddon === "E") {
+			fill(0, 0, 255);
+		}else if(this.coddon === "Q") {
+			fill(120, 120, 0);
+		}else if(this.coddon === "G") {
+			fill(0, 255, 0);
+		}else if(this.coddon === "H") {
+			fill(0, 120, 120);
+		}else if(this.coddon === "I") {
+			fill(255, 255, 0);
+		}else if(this.coddon === "L") {
+			fill(255, 0, 255);
+		}else if(this.coddon === "K") {
+			fill(0, 255, 255);
+		}else if(this.coddon === "<") {
+			fill(123, 231, 32);
+		}else if(this.coddon === "F") {
+			fill(0, 255, 120);
+		}else if(this.coddon === "P") {
+			fill(120, 0, 255);
+		}else if(this.coddon === "S") {
+			fill(155, 255, 0);
+		}else if(this.coddon === "T") {
+			fill(255, 0, 120);
+		}else if(this.coddon === "W") {
+			fill(255, 120, 0);
+		}else if(this.coddon === "Y") {
+			fill(133, 133, 133);
+		}else if(this.coddon === "V") {
+			fill(0, 120, 255);
+		}else if(this.coddon === ">") {
+			fill(0, 21, 152);
+		}else if(this.coddon === "?") {
+			fill(255, 255, 152);
+		}
+		ellipse(this.position.x + offsetX, this.position.y, this.size, this.size);
+		fill(0);
+		textSize(this.size);
+		if(this.coddon === "A") {
+			text("A", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "R") {
+			text("R", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "N") {
+			text("N", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "D") {
+			text("D", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "C") {
+			text("C", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "E") {
+			text("E", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "Q") {
+			text("Q", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "G") {
+			text("G", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "H") {
+			text("H", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "I") {
+			text("I", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "L") {
+			text("L", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "K") {
+			text("K", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "<") {
+			text("<", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "F") {
+			text("F", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "P") {
+			text("P", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "S") {
+			text("S", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "T") {
+			text("T", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "W") {
+			text("W", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "Y") {
+			text("Y", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "V") {
+			text("V", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === ">") {
+			text(">", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}else if(this.coddon === "?") {
+			text("?", this.position.x - this.size * debugKoof + offsetX, this.position.y + this.size * debugKoof);
+		}
 	}
 }
--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-let
-nucleoArray = [],
-nucleoLastId,
-nucleoSize = innerWidth / 150,
-nucleoText,
-memOffset;
-function preload() {
+/** DOM **/
 
-}
-
-function setup() {
-	myCanvas = createCanvas((innerWidth), parseInt(innerHeight) - 45);
-	nucleoLastId = 0;
+let userInput = document.getElementById('userNucleoInput');
+let userOutput = document.getElementById('userCoddonOutput');
+userInput.addEventListener('change', userChangedRNA);
+function userChangedRNA(e) {
 	nucleoArray = [];
 	nucleoText = userInput.value;
 	for(let i = 0; i < nucleoText.length; i++) {
@@ -21,6 +14,92 @@ function setup() {
 		);
 		nucleoLastId++;
 	}
+	
+}
+
+function changeUserInput() {
+	let localText = "";
+	for(let i = 0; i < nucleoArray.length; i++) {
+		localText += nucleoArray[i].letter;
+	}
+	userInput.value = localText;
+	nucleoText = localText;
+	findCoddons();
+}
+
+function resizeBody() {
+	nucleoSize = innerWidth / 150;
+	coddonSize = nucleoSize * 3;
+	setup();
+}
+//** DATABASE **//
+var coddonsDatabase = {
+	"A": ["GCU", "GCC", "GCA", "GCG"],
+	"R": ["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"],
+	"N": ["AAU", "AAC"],
+	"D": ["GAU", "GAC"],
+	"C": ["UGU", "UGC"],
+	"E": ["GAA", "GAG"],
+	"Q": ["CAA", "CAG"],
+	"G": ["GGU", "GGC", "GGA", "GGG"],
+	"H": ["CAU", "CAC"],
+	"I": ["AUU", "AUC", "AUA"],
+	"L": ["CUU", "CUC", "CUA", "CUG", "UUA", "UUG"],
+	"K": ["AAA", "AAG"],
+	"<": ["AUG"],
+	"F": ["UUU", "UUC"],
+	"P": ["CCU", "CCC", "CCA", "CCG"],
+	"S": ["UCU", "UCC", "UCA", "UCG", "AGU", "AGC"],
+	"T": ["ACU", "ACC", "ACA", "ACG"],
+	"W": ["UGG"],
+	"Y": ["UAU", "UAC"],
+	"V": ["GUU", "GUC", "GUA", "GUG"],
+	">": ["UAA", "UAG", "UGA"]
+}
+
+/** P5.JS **/
+let
+nucleoArray = [],
+coddonArray = [],
+targetCoddonArray = [],
+errors = [],
+nucleoLastId,
+coddonLastId,
+targetCoddonLastId,
+nucleoSize = innerWidth / 150,
+coddonSize = nucleoSize * 3,
+nucleoText,
+coddonText,
+targetCoddonText = "<GGSGGSGYQPYRVVVLGGSGGSPYRVVVLSFGGSGGSLSPRWYFYY>",
+memOffset;
+
+function setup() {
+	myCanvas = createCanvas((innerWidth), parseInt(innerHeight) - 90);
+	nucleoLastId = 0;
+	coddonLastId = 0;
+	targetCoddonLastId = 0;
+	nucleoArray = [];
+	coddonArray = [];
+	targetCoddonArray = [];
+	nucleoText = userInput.value;
+	for(let i = 0; i < nucleoText.length; i++) {
+		nucleoArray.push(
+			new Nucleo(
+				nucleoSize + nucleoSize * i, nucleoSize * 2, nucleoLastId, nucleoText.charAt(i), nucleoSize
+			)
+		);
+		nucleoLastId++;
+	}
+	findCoddons();
+	for(let i = 0; i < targetCoddonText.length; i++) {
+		targetCoddonArray.push(
+			new Coddon(
+				nucleoSize * 2 + coddonSize * i, nucleoSize * 2 +  coddonSize * 4 , coddonLastId, targetCoddonText.charAt(i), coddonSize
+			)
+		);
+		targetCoddonLastId++;
+	}
+
 }
 
 function draw() {
@@ -28,7 +107,34 @@ function draw() {
 	for(let i = 0; i < nucleoArray.length; i++) {
 		nucleoArray[i].show();
 	}
+	for(let i = 0; i < coddonArray.length; i++) {
+		coddonArray[i].show();
+	}
+	textSize(coddonSize);
+	fill(255);
+	text("TARGET CODDONS :", width / 2 , nucleoSize * 2 +  coddonSize * 3);
+	for(let i = 0; i < targetCoddonArray.length; i++) {
+		targetCoddonArray[i].show();
+	}
+	checkErrors();
+	showErrors();
+}
 
+/** ERRORS **/
+function checkErrors() {
+	if(!(nucleoArray.length%3)) {
+		errors[0] = 0;
+	}else{
+		errors[0] = 1;
+	}
+}
+
+function showErrors() {
+	if(errors[0] === 1){
+		fill(255, 0, 0);
+		textSize(20);
+		text("Amount of nucleos can`t be devided by 3" , width / 2, height / 2);
+	}
 }
 
 /** MOUSE ITERATIONS **/
@@ -50,8 +156,9 @@ function mouseClicked() {
 }
 
 function mouseWheel(event) {
-	if(nucleoSize - event.delta / 10 > 10) {
+	if(nucleoSize - event.delta / 10 > innerWidth / 200) {
 		nucleoSize -= event.delta / 10;
+		coddonSize = nucleoSize * 3;
 	}
 	setup();
 }
@@ -61,33 +168,34 @@ function mousePressed() {
 function mouseDragged() {
 	offsetX = mouseX - memOffset;
 }
-/** DOM **/
-
-let userInput = document.getElementById('userNucleoInput');
-userInput.addEventListener('change', userChangedRNA);
-function userChangedRNA(e) {
-	nucleoArray = [];
-	nucleoText = userInput.value;
-	for(let i = 0; i < nucleoText.length; i++) {
-		nucleoArray.push(
-			new Nucleo(
-				nucleoSize + nucleoSize * i, nucleoSize * 2, nucleoLastId, nucleoText.charAt(i), nucleoSize
+// Algorithm
+function findCoddons() {
+	coddonText = "";
+	coddonArray = [];
+	for(let j = 0; j < nucleoText.length; j+=3) { // ENTER USER INPUTS
+		let flag = false;
+		for (var key of Object.keys(coddonsDatabase)) {
+			for(let i = 0; i < coddonsDatabase[key].length; i++) {
+				if(
+					coddonsDatabase[key][i].charAt(0) === nucleoText[j] &&
+					coddonsDatabase[key][i].charAt(1) === nucleoText[j + 1] &&
+					coddonsDatabase[key][i].charAt(2) === nucleoText[j + 2]
+				) {
+					coddonText += key;
+					flag = true;
+					break;
+				}
+			}
+		}
+		if(flag === false) {
+			coddonText += "?";
+		}
+	}
+	for(let i = 0; i < coddonText.length; i++) {
+		coddonArray.push(
+			new Coddon(
+				nucleoSize * 2 + coddonSize * i, nucleoSize * 2 +  coddonSize  , coddonLastId, coddonText.charAt(i), coddonSize
 			)
-		);
-		nucleoLastId++;
+		)
 	}
-}
-
-function changeUserInput() {
-	let localText = "";
-	for(let i = 0; i < nucleoArray.length; i++) {
-		localText += nucleoArray[i].letter;
-	}
-	userInput.value = localText;
-	nucleoText = localText;
-}
-
-function resizeBody() {
-	nucleoSize = innerWidth / 150
-	setup();
 }
